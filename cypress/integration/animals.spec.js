d0e4777321f6ab4e5d4a38b7fff2f7c5a82fe631
duaimei <adetwiler@optoro.com>
2020-10-13 22:57:42 -0400

    does it just need to be localhost?

--- cypress/integration/animals.spec.js
+++ cypress/integration/animals.spec.js
@@ -1,6 +1,6 @@
 context('Logging into cypress as a volunteer', () => {
   before(() => {
-    cy.visit('0.0.0.0:3000/')
+    cy.visit('http://localhost:3000/')
   })
   it('should be on the page', () => {
     cy.contains('Animals').should('exist')
